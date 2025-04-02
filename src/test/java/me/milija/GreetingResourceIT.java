c5c454a8e4fa1b5cb40a67fa7e9e5f6f8110e01a
Milija Knežević <milijaknezevic12345@gmail.com>
2025-04-02 18:03:20 +0200

    promjene za bazu

--- src/test/java/me/milija/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package me.milija;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
